fcb761bbfcbc4dacbad2a6819fd79ea4c6e4622e
Arwa Mansoor <arwa.mansoor.khi@gmail.com>
2025-02-18 11:39:55 +0500

    Task 1.cpp

--- Lab 05/Task 1.cpp
+++ Lab 05/Task 1.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class car
+{
+	public:
+	    int* registrationNum;
+	    string* model;
+	    string *ownerName;
+	
+	car(int num, string m, string name)
+	{
+	    registrationNum = new int(num);
+	    model = new  string (m);
+	    ownerName = new string (name);
+	}
+	car(const car &c)
+	{
+	    registrationNum = c.registrationNum;
+	    model = c.model;
+	    ownerName = c.ownerName;
+	}
+	
+	void display()
+	{
+	    cout << "Registration Number :" << *registrationNum << endl;
+	    cout << "Model Name: " << *model << endl;
+	    cout << "Owner Name: " << *ownerName << endl;
+	    cout << "-----------------" << endl;
+	}
+	
+	~car(){}
+};
+
+int main()
+{
+    car c1(122,"cb09","Ali");
+	c1.display();
+    car c2(c1);
+    *c2.ownerName = "Maria";
+	cout << "Car 1 owner's name changed\n";
+	c1.display();
+	cout << "Car 2\n";
+    c2.display();   
+}
 
