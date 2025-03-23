dbb40b03b4a37d339ccd6ae3bb450ec3ccad26b4
Arwa Mansoor <arwa.mansoor.khi@gmail.com>
2025-03-23 15:20:06 +0500

    Task 1.cpp

---
+++ Lab 07/Task 1.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+using namespace std;
+
+class Account
+{
+	protected:	
+		int accountNumber;
+		float balance;
+		string accountHolderName;
+		string accountType;
+		
+	public:	
+		Account(int accountNumber, float balance, string accountHolderName, string accountType) : 
+		accountNumber(accountNumber), balance(balance), accountHolderName(accountHolderName), accountType(accountType){}
+		
+		virtual void deposit(float amount)
+		{
+			balance += amount;
+			cout << "Amount " << amount << " deposited successfuly\n";
+		}
+		
+		virtual void withdraw(float amount)
+		{
+			if(balance < amount)
+			{
+				cout << "Not enough money\n";
+				return;
+			}
+			balance -= amount;
+		}
+		
+		virtual void calculateInterest()
+		{
+			if(balance < 50000)
+			{
+				cout << "Interest: " <<  balance * 0.6 << endl;	
+			}
+			else if(balance < 100000)
+			{
+				cout << "Interest: " <<  balance * 0.9 << endl;
+			}
+			else
+			{
+				cout << "Interest: " <<  balance * 0.12 << endl;
+			}	
+		}
+		
+		void printStatement()
+		{
+			cout << "Account Statement\n";
+			cout << "Account Number: " << accountNumber << endl;
+			cout << "Balance: " << balance << endl;
+		}
+
+		void getAccountInfo()
+		{
+			cout << "Account Number: " << accountNumber << endl;
+			cout << "Account Holder Name: " << accountHolderName << endl;
+			cout << "Balance: " << balance << endl;
+			cout << "Account Type: " << accountType << endl;
+		}
+};
+
+class SavingsAccount : public Account
+{
+	protected:
+		float interestRate;
+		float minimumBalance;
+	
+	public:
+		SavingsAccount(int accountNumber, float balance, string accountHolderName, float interestRate, float minimumBalance) : 
+		Account(accountNumber, balance, accountHolderName, "Savings"),
+		interestRate(interestRate), minimumBalance(minimumBalance){}
+		
+		void calculateInterest()
+		{
+			cout << "Interest: " << balance * interestRate/100 << endl;
+		}
+		
+		void withdraw(float amount)
+		{
+			if(balance - amount < minimumBalance)
+			{
+				cout << "Minimum Balance requirement is not met\n";	
+				return;
+			}
+			Account::withdraw(amount);	
+		}	
+};
+
+class CheckingAccount : public Account
+{
+	protected:
+		float overdraftLimit;
+		
+	public:
+		CheckingAccount(int accountNumber, float balance, string accountHolderName, float overdraftLimit) : 
+		Account(accountNumber, balance, accountHolderName, "Checking"),
+		overdraftLimit(overdraftLimit){}
+	
+		void withdraw(float amount)
+		{
+			if(amount > (balance + overdraftLimit))
+			{
+				cout << "Amount exceeding overdraft limit\n";
+			}
+		}
+};
+
+class FixedDepositAccount : public Account
+{
+	protected:
+		string maturityDate;
+		float fixedInterestRate;
+	
+	public:
+		FixedDepositAccount(int accountNumber, float balance, string accountHolderName, string maturityDate, float fixedInterestRate) : 
+		Account(accountNumber, balance, accountHolderName, "Fixed Deposit"),
+		maturityDate(maturityDate), fixedInterestRate(fixedInterestRate){}
+		
+		void calculateInterest()
+		{
+			cout << "Interest: " << balance * fixedInterestRate/100 << endl;
+		}
+
+		void withdraw(float amount) 
+		{
+			cout << "Money cannot be withdrawn from Fixed Deposit before maturity data\n";
+		}
+};
+
+int main()
+{
+	SavingsAccount sa(12345, 5000, "Mike", 5.4, 1600);
+	sa.deposit(1300);
+	sa.withdraw(500);
+	sa.printStatement();
+	sa.calculateInterest();
+	cout << endl;
+
+	CheckingAccount ca(67890, 3500, "John", 500);
+	ca.deposit(500);
+	ca.withdraw(5000);
+	ca.getAccountInfo();
+	cout << endl;
+
+	FixedDepositAccount fda(13579, 6000, "Sara", "3/24/2025", 7.1);
+	fda.withdraw(1000);
+	fda.getAccountInfo();
+	fda.calculateInterest();
+	cout << endl;
+
+	return 0;
+}
